2b67ecd8ae553239cb11d3f6bdbaaff804ffaf24
Crisandolin Rumahorbo <crisandolin.rumahorbo@btpn.com>
2023-10-12 17:10:21 +0700

    first commit

---
+++ src/main/java/com/bca/vehicleloan/App.java
@@ -0,0 +1,13 @@
+package com.bca.vehicleloan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
